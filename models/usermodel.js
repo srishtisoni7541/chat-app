302dd8e0b370c02dd85f92d4938463f86f589150
srishti soni <srishtisoni7541@gmail.com>
2024-09-22 15:48:16 +0530

    done

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,20 +1,35 @@
-const mongoose=require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/letschat');
 
-const userSchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+const mongoose = require('mongoose');
+
+// Connecting to the MongoDB database
+mongoose.connect('mongodb://127.0.0.1:27017/letschat', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+// Defining the user schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true, // Ensure unique email
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true
     }
-})
+});
 
+// Creating the User model
+const User = mongoose.model('User', userSchema);
 
-module.exports=mongoose.model('user',userSchema);
+// Exporting the model for use in other files
+module.exports = User;
